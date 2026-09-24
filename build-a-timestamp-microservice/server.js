82de0b537ec76616536a3da759a9cd7066d7e43d
riffraff4242 <arifolumo235@gmail.com>
2026-09-24 08:20:21 +0000

    Complete build a timestamp microservice

--- build-a-timestamp-microservice/server.js
+++ build-a-timestamp-microservice/server.js
@@ -15,6 +15,36 @@ app.get("/", (_req, res) => {
 
 // Do not change code below this line
 
+// Route for empty parameter (/api or /api/)
+app.get('/api', (req, res) => {
+  const date = new Date();
+  return res.json({
+    unix: date.getTime(),
+    utc: date.toUTCString()
+  });
+});
+
+// Route for parameterized date (/api/:date)
+app.get('/api/:date', (req, res) => {
+  const dateParam = req.params.date;
+  let date;
+
+  if (/^\d+$/.test(dateParam)) {
+    date = new Date(parseInt(dateParam, 10));
+  } else {
+    date = new Date(dateParam);
+  }
+
+  if (isNaN(date.getTime())) {
+    return res.json({ error: 'Invalid Date' });
+  }
+
+  return res.json({
+    unix: date.getTime(),
+    utc: date.toUTCString()
+  });
+});
+
 const PORT = 8000;
 const listener = app.listen(PORT, function () {
   console.log("Your app is listening on port " + listener.address().port);
